785b76833a9e25b2a9c982278d6e6e14991b9641
varun745633 <iyervarun1820@gmail.com>
2023-12-12 11:30:43 +0530

    tf.auto.tfvars

--- main.tf
+++ main.tf
@@ -14,3 +14,10 @@ resource "azurerm_virtual_network" "tf_vnet" {
   resource_group_name = azurerm_resource_group.tf_rg.name
   address_space       = var.vnet_address_space
 }
+
+resource "azurerm_subnet" "tf_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.tf_rg.name
+  virtual_network_name = azurerm_virtual_network.tf_vnet.name
+  address_prefixes     = var.subnet_address_prefix
+}
---
+++ terraform.auto.tfvars
@@ -0,0 +1,19 @@
+subnet_name = {
+    subnet_1 = {
+       name = "tfc-subnet-1" 
+    }
+
+        subnet_2 = {
+       name = "tfc-subnet-2" 
+    }
+}
+
+subnet_address_prefix = {
+    address_1 = {
+      address_prefixes = "10.0.1.0/26"  
+    }
+
+        address_2 = {
+      address_prefixes = "10.0.2.0/26"  
+    }
+}
